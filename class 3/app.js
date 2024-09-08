7691953a5fecb25312b7333ebd9fbbf1684ae2b2
Muhammad Rayyan <rayyanmaffat@gmail.com>
2024-09-08 17:12:23 +0500

    Add files via upload

---
+++ class 3/app.js
@@ -0,0 +1,73 @@
+// Chapter no 1
+
+
+// Q : no 1
+// alert("Error! Please enter a valid password");
+// Q : no 2
+// alert("Welcome to JS Land... \n Happy coding!");
+// Q : no 3
+// alert("Welcome to JS Land...");
+// Q : no 4
+// alert("Happy Coding");
+// Q : no 5
+// alert("hello... I can run JS through My web browser console");
+
+
+// Chapter no 2
+
+
+// Q : no 4
+
+// var username = "Jhone Doe"
+// alert("Jhon Doe")
+
+// var username = "15 year old"
+// alert("15 year old")
+
+// var username = "Certified mobile application devlopment"
+// alert("Certified mobile application devlopment")
+
+// Q : no 5
+// var username = "PIZZA /n PIZZ /n PIZ /n PI /n P"
+// alert("PIZZA /n PIZZ /n PIZ /n PI /n P")
+
+// Q : no 6
+// var useremail = "My email adress example@example.com"
+// alert("My email adress example@example.com")
+
+// Q : no 7
+// var userpara = "I am trying to learn from the book a smarter \n way to learn javascript"
+// alert("I am trying to learn from the book a smarter \n way to learn javascript")
+
+// Q : no 8
+// document.write("Yah! I can write HTML content through javascript")
+
+// Q : no 9
+// var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+
+// Chapter no 3
+
+
+// Q : no 1
+// var userage = "I am 15 year old"
+// alert("I am 15 year old")
+
+// Q : no 2
+// var userpara = "You have visited the 15 times"
+// alert("You have visited the 15 times")
+
+// Q : no 3
+// document.write("My birth is year 1990 <br> data type of my declare variable is number")
+
+// Q : no 4
+// document.write("Jhon Doe Orderd T-shirt (s) On XYZ Clothing Store")
+
+
+// Chapter no 4
+
+
+// Q : no 3
+document.write("Rules For Naming JS variables <br><br> variables name only contain")
+
