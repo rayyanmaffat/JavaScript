0a5850769de878696a9d573375e8d804d23bdf53
Muhammad Rayyan <rayyanmaffat@gmail.com>
2024-09-12 22:39:11 +0500

    Add files via upload

---
+++ class 5/app.js
@@ -0,0 +1,180 @@
+// Chapter No : 05 
+
+// Question : 01 
+
+
+// var num1 = 3
+// var num2 = 5
+// console.log("the valu is", num1 +  num2 );
+
+
+
+// Question : 02 
+
+// var num1Subtraction = 5
+// var num2Subtraction = 3
+
+// console.log("the Subtraction value is", num1Subtraction - num2Subtraction  );
+
+
+// var num1Multiplication = 5
+// var num2Multiplication = 3
+
+// console.log("the Multiplication value is", num1Multiplication * num2Multiplication  );
+
+
+
+// var num1Division = 5
+// var num2Division = 3
+
+// console.log("the Division value is", num1Division / num2Division  );
+
+
+// var num1Modulus = 5
+// var num2Modulus = 3
+
+// console.log("the Modulus value is", num1Modulus % num2Modulus  );
+
+
+// Question : 03
+
+
+// Question : 03 ( A )
+
+// var a
+
+
+// Question : 03 ( B )
+
+
+// a = "??"
+// console.log(a);
+// a = "hassan"
+// console.log(a);
+
+
+// Question : 03 ( C )
+
+
+// var number = 10
+// console.log(number);
+
+
+// Question : 03 ( D )
+
+
+// var  num = 5
+// console.log(num);
+
+
+// Question : 03 ( E )
+
+
+// var a = 1
+// console.log(a);
+
+
+// Question : 03 ( F )
+
+
+// var a
+// a = 6
+// console.log(a);
+
+
+
+// Question : 03 ( G )
+
+
+// var a = 7
+// console.log(a);
+
+
+// Question : 03 ( H )
+
+
+// var Decrement 
+
+
+// Question : 03 ( I )
+
+
+// var a 
+// a = 12
+// console.log(a);
+
+
+// Question : 03 ( J )
+
+
+// var a =  0
+// var b =  1
+
+
+
+// Question : 03 ( K )
+
+
+// console.log("the reminder is" ,a / b);
+
+
+
+// Question : 03 ( L )
+
+
+// var a = undefined
+// console.log(a);
+
+
+
+// Question : 04
+
+
+// var movieTicketPrice = 600
+// var ticketSell = 5
+// console.log("totel cost of buy 5 ticket to a movie is", ticketSell * movieTicketPrice,"PKR"  );
+
+
+// Question : 05
+
+// var num1 = prompt("Enter val 1" , "Enter Val 1")
+// var num2 = prompt("Enter val 2" , "Enter Val 2")
+
+// document.write(num1 ," X " , num2 ," = " , num1 * num2)
+
+
+// Question : 06
+// var celsius = 30;
+// var celsiusInF = (celsius*9)/5 + 32;
+// console.log(celsius + '°C is ' + celsiusInF + '°F');
+// var fahrenheit = 20;
+// var fahrenheitInC = ((fahrenheit - 32)*5)/9;
+// console.log(fahrenheit + '°F is ' + fahrenheitInC + '°C');
+
+
+// Question : 10
+
+// var birthYear = 1984;
+// var futureYear  = 2012;
+// var age  = futureYear - birthYear;
+// console.log('I will be either ' + age + ' or ' + (age - 1));
+
+
+// Question : 12
+
+
+// var radius = 3;
+// var circumference = Math.PI * 2*radius;
+// console.log("The circumference is " + circumference);
+// var area = Math.PI * radius*radius;
+// console.log("The area is " + area);
+
+
+// Question : 13
+// var age = 28;
+// var maxAge = 100;
+// var numPerDay = 2;
+// var totalNeeded = (numPerDay * 365) * (maxAge - age);
+// var message = 'You will need ' + totalNeeded + ' cups of tea to last you until the ripe old age of ' + maxAge;
+// console.log(message);
+
